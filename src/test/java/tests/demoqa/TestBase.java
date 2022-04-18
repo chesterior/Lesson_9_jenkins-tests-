2224c8c9ed8968ffadcf9f2ebafd3526ca898fef
Aleksandr Safronov <asafronov@sberhealth.ru>
2022-04-18 15:48:00 +0300

    added system more properties tests

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -12,13 +12,29 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
-    @BeforeAll
-    static void setUp() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
+//    @BeforeAll
+//    static void setUp() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//
 //        Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.browserSize = "1920x1080";
 //        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//        Configuration.browserCapabilities = capabilities;
+//    }
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion= System.getProperty("version", "91");
+        Configuration.browserSize= System.getProperty("browserSize", "1960x760");
+        String login = System.getProperty("login","user1");
+        String password = System.getProperty("password","123456");
+        String url = "https://" + login + ":" + password + "@" + "selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = url;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
